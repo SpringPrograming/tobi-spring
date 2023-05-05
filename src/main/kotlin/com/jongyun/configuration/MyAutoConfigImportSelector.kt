0c6346cfb1af858adf2a00e33c3c153775fd3ed5
jongyunha <popawaw@naver.com>
2023-05-05 19:57:17 +0900

    add configuration jetty starter

--- src/main/kotlin/com/jongyun/configuration/MyAutoConfigImportSelector.kt
+++ src/main/kotlin/com/jongyun/configuration/MyAutoConfigImportSelector.kt
@@ -1,6 +1,7 @@
 package com.jongyun.configuration
 
 import com.jongyun.configuration.autoconfig.DispatcherServletConfig
+import com.jongyun.configuration.autoconfig.JettyWebServerConfig
 import com.jongyun.configuration.autoconfig.TomcatWebServerConfig
 import org.springframework.context.annotation.DeferredImportSelector
 import org.springframework.core.type.AnnotationMetadata
@@ -9,6 +10,7 @@ class MyAutoConfigImportSelector : DeferredImportSelector {
     override fun selectImports(importingClassMetadata: AnnotationMetadata): Array<String> {
         return arrayOf(
             TomcatWebServerConfig::class.java.name,
+            JettyWebServerConfig::class.java.name,
             DispatcherServletConfig::class.java.name
         )
     }
--- src/main/kotlin/com/jongyun/configuration/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/com/jongyun/configuration/autoconfig/JettyWebServerConfig.kt
@@ -1,15 +1,15 @@
 package com.jongyun.configuration.autoconfig
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class TomcatWebServerConfig {
+class JettyWebServerConfig {
 
-    @Bean("tomcatServletWebServerFactory")
+    @Bean("jettyServletWebServerFactory")
     fun servletWebServerFactory(): ServletWebServerFactory {
-        return TomcatServletWebServerFactory()
+        return JettyServletWebServerFactory()
     }
 }
